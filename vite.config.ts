c95e705407be8aa97d6ae295ebc31e79c5dd357a
Userhunt <dimburov@mail.ru>
2026-05-29 00:21:48 +0300

    Публикация

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/poe2.re/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
